4b7eaad3bf1a6af8e33e38beb5c87a3eb6783e26
Sebastian Calcan <sebastian95calcan@gmail.com>
2023-01-12 11:36:28 +0200

    Added '/' route for testing 2

--- src/index.js
+++ src/index.js
@@ -8,15 +8,16 @@ const router = new express.Router()
 const app = express()
 const port = process.env.PORT
 
-// Configure Express
-app.use(express.json())     // Automatically parse JSON requests
-app.use(userRouter)         // Use router for user route handlers
-app.use(taskRouter)         // Use router for task route handlers
-
 // Test router
 router.get('/', async(req, res) => {
     res.send('Working')
 })
+
+// Configure Express
+app.use(express.json())     // Automatically parse JSON requests
+app.use(userRouter)         // Use router for user route handlers
+app.use(taskRouter)         // Use router for task route handlers
+app.use(router)             // Use test router
 
 // Start server
 app.listen(port, () => {
